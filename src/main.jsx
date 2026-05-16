f80176180b6edaf52617f2b37a2d5ea27a436093
Nithish-Engineer <nithishreddyb.001@gmail.com>
2026-05-16 09:59:07 +0530

    conflict at main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<<<<<<< HEAD
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-=======
 import { BrowserRouter } from "react-router-dom";
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
->>>>>>> 0aafcf2 (configmap chnages)
   </React.StrictMode>,
 )
